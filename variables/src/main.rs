23626573f2cf95a6efa178855f947087c7ce7c3c
Doug Woodward <doug@fourthpin.com>
2025-03-30 18:30:22 -0700

    variables and mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // give consts all uppercase + _ names
+    let mut x = 5; // mut is keyword to allow variable to be mutable
+    println!("the value of x is: {x}");
+    x = 6;
+    println!("the value of x is: {x}");
+}
