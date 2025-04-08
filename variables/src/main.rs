0cb511697689c899b51274be433e52cfe63c3941
Doug Woodward <doug@fourthpin.com>
2025-04-07 21:02:07 -0700

    little more variables examples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,9 +4,11 @@ fn main() {
     println!("the value of x is: {x}");
     x = 6;
     println!("the value of x is: {x}");
+
+    tuples();
 }
 
-fn floatTypeHints() {
+fn float_type_hints() {
     let x = 2.0; // f64
 
     let y: f32 = 3.0; // f32
@@ -28,4 +30,58 @@ fn artithmetic() {
 
     // remainder
     let remainder = 43 % 5;
+}
+
+fn chars() {
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻'; // rust supports emojis!
+}
+
+fn tuples() {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+
+    let (x, y, z) = tup; // destructuring
+
+    println!("The value of y is: {y}");
+
+    // dot notation
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+}
+
+fn arrays() {
+    let a = [1, 2, 3, 4, 5];
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a: [i32; 5] = [1, 2, 3, 4, 5]; // typed array i32, 5 elements
+    let a = [3; 5]; // [3, 3, 3, ,3, 3]
+
+    //arrays allocate on the stack
+    //arrays are size imutable!
+    //if you want to grow/shrink, use vector instead
+
+    //array accessing
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+    // rust will handle index out of bounds by panicking, throwing err and exiting
 }
