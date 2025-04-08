021b5cd22380ae7c8d8fb0ea0e885332e8fe04ba
Doug Woodward <doug@fourthpin.com>
2025-04-07 20:55:10 -0700

    adding some more notes on types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -5,3 +5,27 @@ fn main() {
     x = 6;
     println!("the value of x is: {x}");
 }
+
+fn floatTypeHints() {
+    let x = 2.0; // f64
+
+    let y: f32 = 3.0; // f32
+}
+
+fn artithmetic() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+}
