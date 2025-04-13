9e16daef827bcf179ffe676809f2e7146951307e
Doug Woodward <doug@fourthpin.com>
2025-04-12 20:12:52 -0700

    note on expressions vs statements

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
     println!("Hello, world!");
     another_function(42, "psi");
+    let y = {
+        let x = 3;
+        x + 1 // note the lack of semicolon here, that makes it an expression! not a statement,
+              // therefor it returns a value!
+    }; // y is 4 because the function returns x + 1 which is 4
+    println!("{y}");
+    five();
 }
 
 fn another_function(value: i32, unit_label: &str) {
     println!("value: {value} {unit_label}");
+}
+
+fn five() -> i32 {
+    5 // this is the return value implicit. again, not semi colon!
 }
