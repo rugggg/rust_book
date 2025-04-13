6d2884fcc549d3b31d7808cdbe998a9efbdc44da
Doug Woodward <doug@fourthpin.com>
2025-04-12 19:59:14 -0700

    multi value funcs

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(42, "psi");
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(value: i32, unit_label: &str) {
+    println!("value: {value} {unit_label}");
 }
